07d2ec92c073c77284e3169816e1275532be6367
Princekumarsingh58999 <108799184+Princekumarsingh58999@users.noreply.github.com>
2023-08-20 07:43:05 +0530

    Time: 3 ms (42.90%), Space: 6.2 MB (22.99%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -1,20 +1,36 @@
 class Solution {
 public:
     string interpret(string command) {
-      string s="",str;
-        for(auto ch:command){
-            s+=ch;
-            if(s=="G"){
-                str+="G";
-            s="";}
-            else if(s=="()"){
-                str+="o";
-                s="";}
+//       string s="",str;
+//         for(auto ch:command){
+//             s+=ch;
+//             if(s=="G"){
+//                 str+="G";
+//             s="";}
+//             else if(s=="()"){
+//                 str+="o";
+//                 s="";}
             
-                    else if(s=="(al)"){
-                    str+="al";
-                        s="";}
+//                     else if(s=="(al)"){
+//                     str+="al";
+//                         s="";}
+//         }
+//                     return str;
+        
+        string s;
+        for(int i=0;i<command.size();i++){
+            if(command[i]=='G')
+             s.push_back('G');
+            else if(command[i]=='(' ){
+                if(command[i+1]==')')
+                s.push_back('o');
+            else
+            {
+                s.push_back('a');
+                s.push_back('l');
+            }
+    }
         }
-                    return str;
+        return s;
     }
 };
