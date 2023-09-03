7b2b6ed7594944ef57f07deac26cd4163fa1c9f3
Princekumarsingh58999 <108799184+Princekumarsingh58999@users.noreply.github.com>
2023-09-03 23:00:59 +0530

    Time: 15 ms (12.88%), Space: 10.1 MB (69.50%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -4,7 +4,7 @@ public:
 //         set<int> v;
        
 //         for(int i=0;i<nums1.size();i++){
-//             for(int j=i;j<nums2.size();j++){
+//             for(int j=0;j<nums2.size();j++){
 //             if(nums1[i]==nums2[j]){
 //                 v.insert(nums1[i]);
 //                break;
