40970d636a675588f6c5eb50c939d0b9e66667ed
Princekumarsingh58999 <108799184+Princekumarsingh58999@users.noreply.github.com>
2023-09-06 19:47:56 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (93.88%) - LeetHub

--- 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.cpp
+++ 2180-count-integers-with-even-digit-sum/2180-count-integers-with-even-digit-sum.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
+    bool check(int num)
+        {
+        int sum=0;
+             while(num>0){
+                sum+=num%10;
+                num/=10;
+                     
+            }
+        return sum%2;   
+    }
+        
+    
     int countEven(int num) {
-//         int count=0;
+        int count=0;
       
-//         for(int i=2;i<=num;i++){
-//             if((i/10)%2==0 && (i%100)%2==0){
-//             count++;}
-//             else if((i/10)%2!=0 && (i%100)%2!=0){
-//                 count++;
-//             }
+        for(int i=2;i<=num;i++){
+            // if((i/10)%2==0 && (i%100)%2==0){
+            // count++;}
+            // else if((i/10)%2!=0 && (i%100)%2!=0){
+            //     count++;
+            
+            if(!check(i))
+                count++;
+            }
            
-//         }
-//         return count;
-       int sum=0;
-        int p=num;
-        while(num>0){
-            sum+=num%10;
-            num/=10;
-        }
-        if(sum%2==0)
-            return p/2;
-        else return (p-1)/2;
+        return count;
     }
 };
