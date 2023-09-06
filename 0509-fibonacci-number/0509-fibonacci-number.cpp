e1f71659258f8804ca0ba0d75cc6841bc573609f
Princekumarsingh58999 <108799184+Princekumarsingh58999@users.noreply.github.com>
2023-09-06 20:00:38 +0530

    Time: 2 ms (74.32%), Space: 6.2 MB (8.78%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -5,8 +5,14 @@ public:
             return 1;
         else if(n==0)
             return 0;
-       else
-            return fib(n-1)+fib(n-2);
-       
+       // else
+       //      return fib(n-1)+fib(n-2);
+       vector<int> arr(n+1);
+        arr[0]=0;
+        arr[1]=1;
+        
+        for(int i=2;i<=n;i++)
+            arr[i]=arr[i-1]+arr[i-2];
+        return arr[n];
     }
 };
