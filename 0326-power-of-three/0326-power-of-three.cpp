bc62eea640afbcab0c01ec855fd026b012da0ad4
Princekumarsingh58999 <108799184+Princekumarsingh58999@users.noreply.github.com>
2023-09-13 22:18:59 +0530

    Time: 8 ms (85.87%), Space: 6.3 MB (9.38%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-                long long int p=n;
-        if(n<=0)
-            return false;
-        else if(n==1)
-            return true;
-       else{
-        while(p!=0){
-            if(p%3!=0){
-                return false;
-                break;}
-            else if(p%3==0){
-                p=p/3; 
-             if(p==1)
-        return true;}
-        }
+//                 long long int p=n;
+//         if(n<=0)
+//             return false;
+//         else if(n==1)
+//             return true;
+//        else{
+//         while(p!=0){
+//             if(p%3!=0){
+//                 return false;
+//                 break;}
+//             else if(p%3==0){
+//                 p=p/3; 
+//              if(p==1)
+//         return true;}
+//         }
                    
-       }
+//        }
 
-        return false;
+//         return false;
+       
         
+   //largest number for divesible to 3 in bitwine 2^31. which is given in question.     
+        return (n>0 && 1162261467%n==0);
     }
 };
