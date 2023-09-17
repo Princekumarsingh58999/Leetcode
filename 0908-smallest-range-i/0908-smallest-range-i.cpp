322c595d4e2acb08818e6ced96114c6ede5090a1
Princekumarsingh58999 <108799184+Princekumarsingh58999@users.noreply.github.com>
2023-09-17 07:41:28 +0530

    Time: 12 ms (72.86%), Space: 15.8 MB (5.02%) - LeetHub

--- 0908-smallest-range-i/0908-smallest-range-i.cpp
+++ 0908-smallest-range-i/0908-smallest-range-i.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     int smallestRangeI(vector<int>& nums, int k) {
-        int n=nums.size();
-        sort(nums.begin(),nums.end());
-        if(nums.size()==1)
-            return 0;
+//         int n=nums.size();
+//         sort(nums.begin(),nums.end());
+//         if(nums.size()==1)
+//             return 0;
        
-            int p=nums[0]+k;
-           int  q=nums[n-1]-k;
-             if(p<=q)
-                return q-p;
-            else return 0;
+//             int p=nums[0]+k;
+//            int  q=nums[n-1]-k;
+//              if(p<=q)
+//                 return q-p;
+//             else return 0;
+        
+        int p=*max_element(nums.begin(),nums.end());
+        int q=*min_element(nums.begin(),nums.end());
+        return max(0,((p-k)-(q+k)));
         
     }
 };
